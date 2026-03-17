e242ec33426481cedc5dc0d87967026c0507be50
Aaditya Choudhary <aadityach812@gmail.com>
2026-03-17 16:10:11 +0530

    second change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react'
+import Link from 'next/link';
 
 const page = () => {
   const [dog , setDog] = useState<string>("");
@@ -29,16 +30,23 @@ const page = () => {
     fetchDog();
   },[])
   return (
-    <div className='bg-[#E1D0C1] flex flex-col justify-center items-center min-h-screen'>
-      <h1 className='text-4xl text-gray-600 font-bold my-8'>Random Dog Generator !</h1>
+    <div className='bg-[#E1D0C1]'>
+        <Link href="/movies">
+        <button className='m-4 text-black  px-2 py-2 text-3xl hover:cursor-pointer  '>
+            →
+        </button>
+        </Link>
+        <div className=' flex flex-col justify-center items-center min-h-screen'>
+          <h1 className='text-4xl text-gray-600 font-bold my-8'>Random Dog Generator !</h1>
 
-      {loading && <p className=' text-gray-600 mb-8'> Loading cute dog ...</p> }
+          {loading && <p className=' text-gray-600 mb-8'> Loading cute dog ...</p> }
 
-      {!loading && dog && (
-        <img className='w-80 h-80 object-cover rounded-lg shadow-lg mb-8' src={dog} alt="dog image" />
-      )}
+          {!loading && dog && (
+            <img className='w-80 h-80 object-cover rounded-lg shadow-lg mb-8' src={dog} alt="dog image" />
+          )}
 
-      <button className='bg-black text-white rounded-xl px-4 py-2 hover:bg-gray-600 hover:font-bold' onClick={fetchDog}>click for Dog 🐾</button>
+          <button className='bg-black text-white rounded-xl px-4 py-2 hover:bg-gray-600 hover:font-bold' onClick={fetchDog}>click for Dog 🐾</button>
+        </div>
     </div>
   )
 }
